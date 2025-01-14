dee537c4df4b1339d3313a6442ef46ec4e6ce2e7
Takmim00 <takmimm@gmail.com>
2025-01-14 20:15:17 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-require('dotenv').config()
-const express = require('express');
-const app = express()
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-
-
 
 //middleware
 app.use(cors());
-app.use(express.json()); 
-
-
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcial.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,14 +26,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    //jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
 
-
-
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -47,11 +48,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("studyHive is running");
+});
 
-app.get('/', (req,res)=>{
-    res.send('studyHive is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Study Hive is running on pot ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Study Hive is running on pot ${port}`);
+});
