3767cf68d9ffc12a632c8c4b9730979bcecf58ad
Takmim00 <takmimm@gmail.com>
2025-01-18 11:00:28 +0600

    note delet method added

--- index.js
+++ index.js
@@ -197,6 +197,12 @@ async function run() {
       const result = await noteCollection.find(query).toArray();
       res.send(result);
     });
+    app.delete("/notes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await noteCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
